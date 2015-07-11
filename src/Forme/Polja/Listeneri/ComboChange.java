c33a8b346e1b776cb3c0bf9a08d221d62e2056ac
klapnebojsa <klapnebojsa@gmail.com>
2015-07-12 00:36:30 +0200

    poruka01_00411

--- src/Forme/Polja/Listeneri/ComboChange.java
+++ src/Forme/Polja/Listeneri/ComboChange.java
@@ -5,25 +5,12 @@
  */
 package Forme.Polja.Listeneri;
 
-import Forme.Konstante.FunkcijskiTasteri;
-import Forme.Polja.Prikazi.PoljaIzTabeleDefinicija;
-import Forme.Polja.Prikazi.PoljaIzTabeleNapuni;
-import Forme.PopUpovi.TablePopUp;
-import Forme.Tabele.MojaTabela;
 import Sistem.OsnovneDefinicije.RezolucijaEkrana;
 import Stampa.PreviewMenuBar;
-import java.awt.AWTException;
 import java.awt.Dimension;
-import java.awt.Robot;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JMenuItem;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 /**
  *
  * @author Nebojsa
@@ -39,30 +26,30 @@ public class ComboChange implements ItemListener {
 
     Object vrednost = null;
     public void itemStateChanged(ItemEvent event) {
-       if (event.getStateChange() == ItemEvent.SELECTED) {
-          vrednost = event.getItem();
-          try {
-            previewMenuBar.tekuceUvecanje.setText(vrednost.toString());
-            try{
-                vrednost = vrednost.toString().replace(" %", "");
-                previewMenuBar.formPrintPreview.prikazi.p = Double.parseDouble(vrednost.toString()) / 100;                
-            }catch(Exception e){
-                RezolucijaEkrana re = new RezolucijaEkrana();
-                Dimension fullScr = re.FullScreen();
-                
-                Dimension prefSize = previewMenuBar.formPrintPreview.prikazi.getPreferredSize();
-                switch (vrednost.toString()){
+        if (event.getStateChange() == ItemEvent.SELECTED) {
+            vrednost = event.getItem();
+            try {
+                previewMenuBar.tekuceUvecanje.setText(vrednost.toString());
+                try{
+                    vrednost = vrednost.toString().replace(" %", "");
+                    previewMenuBar.formPrintPreview.prikazi.p = Double.parseDouble(vrednost.toString()) / 100;                
+                }catch(Exception e){
+                    RezolucijaEkrana re = new RezolucijaEkrana();
+                    Dimension fullScr = re.FullScreen();
+
+                    Dimension prefSize = previewMenuBar.formPrintPreview.prikazi.getPreferredSize();
+                    switch (vrednost.toString()){
                         case "page Width":
                             previewMenuBar.formPrintPreview.prikazi.p = (double)fullScr.width / prefSize.width;
                             break;
                         case "page Height":
                             previewMenuBar.formPrintPreview.prikazi.p = (double)fullScr.height / prefSize.height;                            
                             break;
+                    }
                 }
-            }
-            previewMenuBar.formPrintPreview.revalidate();
-            previewMenuBar.formPrintPreview.repaint();   
-          }catch(Exception e1){}
+                previewMenuBar.formPrintPreview.revalidate();
+                previewMenuBar.formPrintPreview.repaint();   
+            }catch(Exception e1){}
        }
     }
 }
